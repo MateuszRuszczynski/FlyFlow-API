767d490941adff24a8d497b6d674010d802fbf97
Mateusz <mateusz.ruszczynski@gmail.com>
2026-05-05 14:30:25 +0200

    feat: add new endpoints

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/user/", include("users.urls"), namespace="user"),
+    path("api/airport/", include("airport.urls"), namespace="airport")
 ]
