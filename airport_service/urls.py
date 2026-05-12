c7075bbef78aa322469c9cd218a18f0efe186620
Mateusz <mateusz.ruszczynski@gmail.com>
2026-05-12 15:56:08 +0200

    feat: add static files

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("users.urls", namespace="user")),
     path("api/airport/", include("airport.urls", namespace="airport"))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
